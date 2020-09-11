2c0aff10d7f7d178e5fde8c6946d7692470465c7
HongHuiXing <1390416863@qq.com>
2020-09-11 15:53:59 +0800

    abcd

--- eurekaregister/src/main/java/cn/china/eurekaregister/EurekaregisterApplication.java
+++ eurekaregister/src/main/java/cn/china/eurekaregister/EurekaregisterApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaServer
+// haha
 public class EurekaregisterApplication {
 
     public static void main(String[] args) {
